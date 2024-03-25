593e138dc951e18e51407aa54eec2bb15e9863d3
David G <david.giasson@claurendeau.qc.ca>
2024-03-25 18:06:18 -0400

    Remaniement des tests unitaires.

--- src/ListeTest.java
+++ src/ListeTest.java
@@ -9,55 +9,56 @@ class ListeTest {
     @BeforeEach
     void setUp() {
         data = new Liste();
-        data.ajouter(0);
-        data.ajouter(10);
-        data.ajouter(20);
-        data.ajouter(30);
-        data.ajouter(40);
-        data.ajouter(50);
-    }
-
-    @Test
-    void getElementAt() {
-        assertEquals(0, data.getElementAt(0));
-        assertEquals(30, data.getElementAt(3));
-        assertEquals(50, data.getElementAt(5));
+        data.ajouter(1);
+        data.ajouter(2);
+        data.ajouter(3);
+        data.ajouter(4);
+        data.ajouter(5);
     }
 
     @Test
     void getNbElements() {
+        assertEquals(5, data.getNbElements());
+
         Liste vide = new Liste();
         assertEquals(0, vide.getNbElements());
-        assertEquals(6, data.getNbElements());
     }
 
     @Test
     void estVide() {
+        assertFalse(data.estVide());
+
         Liste vide = new Liste();
-        assertEquals(true, vide.estVide());
-        assertEquals(false, data.estVide());
+        assertTrue(vide.estVide());
+    }
+
+    @Test
+    void getElementAt() {
+        assertEquals(1, data.getElementAt(0));
+        assertEquals(3, data.getElementAt(2));
+        assertEquals(5, data.getElementAt(4));
     }
 
     @Test
     void testAjouter() {
-        data.ajouter(90);
-        data.ajouter(91);
-        data.ajouter(92);
-        assertEquals(90, data.getElementAt(6));
-        assertEquals(91, data.getElementAt(7));
-        assertEquals(92, data.getElementAt(8));
-        assertEquals(9, data.getNbElements());
+        data.ajouter(95);
+        data.ajouter(96);
+        data.ajouter(97);
+        assertEquals(8, data.getNbElements());
+        assertEquals(95, data.getElementAt(5));
+        assertEquals(96, data.getElementAt(6));
+        assertEquals(97, data.getElementAt(7));
     }
 
     @Test
     void testInserer() {
-        data.ajouter(-10, 0);
-        data.ajouter(35, 4);
-        data.ajouter(60, 8);
-        assertEquals(-10, data.getElementAt(0));
-        assertEquals(35, data.getElementAt(4));
-        assertEquals(60, data.getElementAt(8));
-        assertEquals(9, data.getNbElements());
+        assertTrue(data.ajouter(90, 0));
+        assertTrue(data.ajouter(94, 4));
+        assertTrue(data.ajouter(97, 7));
+        assertEquals(8, data.getNbElements());
+        assertEquals(90, data.getElementAt(0));
+        assertEquals(94, data.getElementAt(4));
+        assertEquals(97, data.getElementAt(7));
     }
 
     @org.junit.jupiter.api.Test
